9cfa84b5e3b2421544435feec3cd15dddde948f3
Bonnie Liu <bonnieliu2002@gmail.com>
2021-04-21 18:16:07 -0700

    plotted noncentral t-distribution for hw4_3.R

--- hw4_3.R
+++ hw4_3.R
@@ -3,21 +3,31 @@ a1 <- read.table("http://www.stat.ucla.edu/~nchristo/statistics100C/soil.txt", h
 
 # n is the number of observations
 n = 30
-
 # only want to use first 30 rows of data set
 a <- a1[1:n, ]
 
-names(a)
-
 # apply linear model with lead as our response variable and zinc as predictor variable
 q <- lm(a$lead ~ a$zinc)
+summary(q)
 
-# b1 is Beta_1_hat
-b1 <- q$coef[2]
-# Se is the square root of sample variance
-Se <- summary(q)$sigma
+# central t-distribution
+x <- seq(-10, 10, 0.5)
+y <- dt(x, n-2, ncp=0)
+plot(x, y, type="l", lwd=5, ylab=substitute(f(x)), xlim=c(-11,11))
 
-# t-statistic
-t <- b1 / (Se/sqrt(sum((a$zinc - mean(a$zinc))^2))) # calculated from equation from 3a
-print(t) # prints 18.22474
-print(2 * (1 - pt(t, n-1))) # two-side t-test; prints 0
+# # noncentral t-distribution
+# beta1 <- 0.05
+# sigma2 <- 600
+# ncp <- (beta1-0)*sqrt()
+# 
+# print(var(a$zinc))
+
+# # b1 is Beta_1_hat
+# b1 <- q$coef[2]
+# # Se is the square root of sample variance
+# Se <- summary(q)$sigma
+# 
+# # t-statistic
+# t <- b1 / (Se/sqrt(sum((a$zinc - mean(a$zinc))^2))) # calculated from equation from 3a
+# print(t) # prints 18.22474
+# print(2 * (1 - pt(t, n-1))) # two-side t-test; prints 0
